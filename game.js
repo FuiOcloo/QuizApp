0d3f810e426476a6ff437b37ff3731f2d835f932
FuiOcloo <fui.ocloo@amalitech.org>
2020-10-09 01:26:55 +0000

    Create a spinning loader

--- game.js
+++ game.js
@@ -1,8 +1,10 @@
-const question = document.getElementById('question');
-const choices = Array.from(document.getElementsByClassName('choice-text'));
+const question = document.getElementById("question");
+const choices = Array.from(document.getElementsByClassName("choice-text"));
 const progressText = document.getElementById("progressText");
 const scoreText = document.getElementById("score");
 const progressBarFull = document.getElementById("progressBarFull");
+const loader = document.getElementById("loader");
+const game = document.getElementById("game");
 let currentQuestion = {};
 let acceptingAnswers = false;
 let score = 0;
@@ -38,6 +40,7 @@ fetch(
 
             return formattedQuestion;
         });
+    
         startGame();
     })
     .catch((err) => {
@@ -53,6 +56,8 @@ startGame = () => {
     score = 0;
     availableQuesions = [...questions];
     getNewQuestion();
+    game.classList.remove('hidden');
+    loader.classList.add('hidden');
 };
 
 getNewQuestion = () => {
